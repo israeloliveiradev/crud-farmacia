17f9895c626f7230e3c94d6849608ba39011eb5f
Israel <israeloliveiracontact@gmail.com>
2024-07-22 09:24:24 -0300

    :tada: first commit

---
+++ src/test/java/com/israeloliveira/crud_farmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israeloliveira.crud_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
